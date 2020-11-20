a7c8526c8ea693365d0ab660244586384825e880
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2020-11-20 12:15:46 +0100

    AGTB-10: Resolve conflicts

--- sugarcrm/custom/Extension/application/Ext/Include/gtb_contacts.php
+++
@@ -1,7 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-$beanList['gtb_contacts'] = 'gtb_contacts';
-$beanFiles['gtb_contacts'] = 'modules/gtb_contacts/gtb_contacts.php';
-$moduleList[] = 'gtb_contacts';
-
-?>
